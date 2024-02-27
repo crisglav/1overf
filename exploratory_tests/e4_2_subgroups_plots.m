fdf8635cc6f00f23c777f1a7d0fcfad9ea4a4e58
Cristina Gil <cristina.gil@tum.de>
2024-02-27 17:58:20 +0100

    added e4

--- exploratory_tests/e4_2_subgroups_plots.m
+++ exploratory_tests/e4_2_subgroups_plots.m
@@ -1,19 +1,109 @@
-Compare aperiodic exponents between healthy participants and the two
-biggests subgroups of patients (Chronic back pain patients = CBP) and
-chronic widespread pain patients (CWP)
+% Compare aperiodic exponents between healthy participants and the two
+% biggests subgroups of patients (Chronic back pain patients = CBP) and
+% chronic widespread pain patients (CWP). Generate raincloudplots
 %
 % Cristina Gil, Flaminia Palloti, TUM, 27.02.2024
 
-% Load data from aperiodic exponents in the mPFC
 
 clear all, close all,
 
-datapath = '../../results/statistics/exp_PFC_real.csv';
-data = readtable(datapath);
-data.group = categorical(data.group);
-data.diagnosis = categorical(data.diagnosis);
+% Add raincloudplots
+addpath('../../toolboxes/raincloudplots');
 
-% Separate participants by group and diagnosis
-hc_mask = data.group == 'hc';
-cbp_mask = data.diagnosis == 'CBP';
-cwp_mask = data.diagnosis == 'CWP';
+% Path for the figures
+figures_path = '../../results/figures';
+
+% Load prepare data from csv. First you have to run
+% e4_1_subgroups_statistics.R
+datapath = '../../results/statistics';
+cwp = readtable(fullfile(datapath,'e4_cwp.csv'));
+cbp = readtable(fullfile(datapath,'e4_cbp.csv'));
+cwp.group = categorical(cwp.group);
+cbp.group = categorical(cbp.group);
+
+% Separate residuals per group
+cwp_hc_mask = cwp.group == 'hc';
+cwp_pa_mask = cwp.group == 'pa';
+apexp_cwp_hc = cwp.res_apexp(cwp_hc_mask);
+apexp_cwp_pa = cwp.res_apexp(cwp_pa_mask);
+
+cbp_hc_mask = cbp.group == 'hc';
+cbp_pa_mask = cbp.group == 'pa';
+apexp_cbp_hc = cbp.res_apexp(cbp_hc_mask);
+apexp_cbp_pa = cbp.res_apexp(cbp_pa_mask);
+
+
+%% Hypothesis 1: Do aperiodic offsets in the mPFC differ between patients and healthy participants?
+
+% Plot aperiodic offsets for patients and healthy participants
+f1 = figure('Units','centimeters','Position',[25 25 22 20]);
+tiledlayout(2,2);
+ax=nexttile;
+h1 = raincloud_plot_vertical(apexp_cbp_hc,'box_dodge',1, 'color', '#69A5C4', 'alpha', 1, 'bxcl', [.2 .2 .2], 'line_width', 1.5, 'box_dodge_amount', 0.5,'dot_dodge_amount',0.5,'wdth',0.3);
+h2 = raincloud_plot_vertical(apexp_cbp_pa, 'box_dodge', 1, 'color', "#D95D4A", 'alpha',1, 'bxcl', [.2 .2 .2], 'line_width', 1.5, 'box_dodge_amount', 1,'dot_dodge_amount',1,'wdth',0.3);
+xlim([-3, 1.5])
+ylim([-0.8 0.8])
+legend([h1{1} h2{1}], {'matched HC', 'CBP'},'Location','southwest');
+title('CBP patients')
+set(ax,'XTick',[],'XTickLabel',[]);
+ylabel('Aperiodic exponents residuals');
+box off
+
+ax = nexttile;
+% xlim([-3, 1.5])
+h1 = raincloud_plot_vertical(apexp_cwp_hc,'box_dodge',1, 'color', '#69A5C4', 'alpha', 1, 'bxcl', [.2 .2 .2], 'line_width', 1.5, 'box_dodge_amount', 0.5,'dot_dodge_amount',0.5,'wdth',0.3);
+h2 = raincloud_plot_vertical(apexp_cwp_pa, 'box_dodge', 1, 'color', "#D9B64A", 'alpha',1, 'bxcl', [.2 .2 .2], 'line_width', 1.5, 'box_dodge_amount', 1,'dot_dodge_amount',1,'wdth',0.3);
+xlim([-3, 1.5])
+ylim([-0.8 0.8])
+legend([h1{1} h2{1}], {'matched HC', 'CWP'},'Location','southwest');
+title('CWP patients')
+set(ax,'XTick',[],'XTickLabel',[]);
+box off
+% saveas(f1,fullfile(figures_path,'e4_subgroups_h1.svg'));
+
+
+
+%% Hypothesis 2: Do aperiodic offsets in the mPFC correlate with pain ratings in patients?
+% Plot aperiodic offsets for patients and healthy participants
+% f2 = figure('Units','centimeters','Position',[25 25 22 9]);
+% tlc = tiledlayout(1,2);
+ax=nexttile;
+model_cbp = fitlm(cbp.res_pain(cbp_pa_mask), cbp.res_apexp(cbp_pa_mask));
+h = plot(model_cbp);
+h(1).Marker = 'o';
+h(1).MarkerFaceColor = '#D95D4A';
+h(1).MarkerEdgeColor = 'none';
+h(2).Color = [0.5 0.5 0.5];
+h(2).LineWidth = 1.5;
+h(3).Color = [0.5 0.5 0.5];
+h(3).LineWidth = 1.5;
+h(4).Color = [0.5 0.5 0.5];
+h(4).LineWidth = 1.5;
+xlabel('Pain residuals');
+ylabel('Aperiodic exponent residuals');
+title('CBP patients');
+box off;
+legend off;
+
+model_cwp = fitlm(cwp.res_pain(cwp_pa_mask), cwp.res_apexp(cwp_pa_mask));
+nexttile;
+h = plot(model_cwp);
+h(1).Marker = 'o';
+h(1).MarkerFaceColor = '#D9B64A';
+h(1).MarkerEdgeColor = 'none';
+h(2).Color = [0.5 0.5 0.5];
+h(2).LineWidth = 1.5;
+h(3).Color = [0.5 0.5 0.5];
+h(3).LineWidth = 1.5;
+h(4).Color = [0.5 0.5 0.5];
+h(4).LineWidth = 1.5;
+xlabel('Pain residuals');
+% ylabel('Aperiodic exponent residuals');
+ylabel('')
+title('CWP patients');
+box off;
+legend off;
+ylim([- 0.6 0.6])
+
+% saveas(f2,fullfile(figures_path,'e4_subgroups_h2.svg'));
+saveas(f2,fullfile(figures_path,'e4_subgroups.svg'))
